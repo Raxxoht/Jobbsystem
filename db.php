4bedf07b72825df22cbb83c8536d9ec880162a1a
Oskar <Oskar.hauge123@gmail.com>
2023-10-17 18:45:03 +0200

    Laget php fil for databasetilkobling

---
+++ db.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    //This file is a db initialization file and will be included in every file requiring a db connecting
+</body>
+</html>
