5f879a28398a95aa6e8e913c77d934ce4721f280
Oskar <Oskar.hauge123@gmail.com>
2023-10-17 20:51:28 +0200

    Laget en databasekoblingsfil og startet på filstruktur

--- db.php
+++ db.php
@@ -6,6 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    //This file is a db initialization file and will be included in every file requiring a db connecting
+    <?php
+
+    //This file is a db initialization file and will be included in every file requiring a db connection
+    $username = "root"; #Legger inn en variabel for å skrive inn brukernavn i myphpadmin
+    $password = ""; #Legger inn variabel for passordet i phpadmin
+    $servername = "127.0.0.1:4306"; #Lager variabel "host" for servernavn, må være port og IP hos OSKAR
+    $database = "jobbsystem"; #Lager variabel for databasenavnet
+    $conn = new mysqli($servername, $username,$password, $database); #Etablerer variabel for koblingen til sql serveren
+
+    if($conn->connect_errno) { #Sjekker om det finnes en feil i koblingen
+        echo "Connection Failed" . $conn->connect_error;
+        exit();
+    }
+    echo "Connection success"; //Successful connection
+    $conn->close(); //Lukker koblingen for å hindre memory leak
+
+    ?>  
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+    include "db.php";
+    ?>
 </body>
 </html>
