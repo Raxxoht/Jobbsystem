3800e578632591d0c547711f6f7aae39dae5ae64
Raxxo <Oskar.Hauge123@gmail.com>
2023-10-24 21:00:10 +0200

    Laget startside, navigasjonsbar og stylet dem etter egne tanker

--- www/index.php
+++ www/index.php
@@ -4,8 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./Assets./Css/style.css">
 </head>
-<body>
-    <h1>Velkommen til vår hovedside</h1>
+<body id="startBody">
+    <div id="NavBar">
+        <a class="NavItems" href="./index.php">Start</a>
+        <a class="NavItems" href="./Pages/Hovedside/Hovedside.php">Hovedside</a>
+        <a class="NavItems" href="./Pages/Soknadside/Soknad.php">Søknader</a>
+        <a class="NavItems" href="./Pages/Profilside/Profil.php">Profil</a>
+    </div>
+    <div id="Main_Content">
+        <h1>Velkommen til Jobbsøkesystemet vårt!</h1>
+    </div>
+    <!--<section id="Footer">
+
+    </section> Mulig Footer-->
 </body>
 </html>
