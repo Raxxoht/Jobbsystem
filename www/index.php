6511b37d0c5e2663e430d964eb7e804f1183392a
Oskar <Oskar.hauge123@gmail.com>
2023-11-15 14:38:31 +0100

    Laget en registreringsside samt satt opp kobling til den fra loginsida

--- www/index.php
+++ www/index.php
@@ -3,22 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Start</title>
     <link rel="stylesheet" href="./Assets./Css/style.css">
 </head>
+
 <body id="startBody">
+
     <?php
     include "./Assets/Html/navbar.php";
     ?>
     <div id="loginBox">
         <h1>Login</h1>
         <form action="" method="POST">
+
             Brukernavn <input placeholder="Skriv inn brukernavn her" type="text">
+            
             Passord <input placeholder="Skriv inn passord her" type="text">
+
             <button type="submit">Send inn</button>
         </form>
 
-        <a href="">registrer ny bruker her</a>
+        <a href="./Pages/Registrer/Registrer.php">registrer ny bruker her</a>
     </div>
 </body>
 </html>
