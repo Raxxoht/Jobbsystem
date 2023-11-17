0f7f7910b27931a85c9072f644023078970c8fcf
Oskar <Oskar.hauge123@gmail.com>
2023-11-17 16:55:05 +0100

    Fikset opp i registrering slik at den endrer på skjemaet etter brukertype

--- www/Pages/Registrer/Registrer.php
+++ www/Pages/Registrer/Registrer.php
@@ -9,12 +9,24 @@
 <body id="startBody">
     <div id="regFormBox">
         <form action="" method="POST" id="regForm">
-            Brukernavn <input placeholder="Skriv inn brukernavn" name="regBNavn" type="text"> <br>
-            Passord <input placeholder="Skriv inn passord" name="regPass"  type="text"> <br>
-            Fornavn <input placeholder="Skriv inn fornavn" type="text" name="regFNavn"> <br>
-            Etternavn <input placeholder="Skriv inn etternavn" type="text" name="regENavn"> <br>
-            Fødselsdato <input type="date" name="regFDato"> <br>
-            Telefonummer <input type="tel" name="regTlf"> <br>
+            <?php 
+                if($_GET["Type"]=="arbeidssoker"){
+                    echo 'Brukernavn <input placeholder="Skriv inn brukernavn" name="regBNavn" type="text"> <br>
+                    Passord <input placeholder="Skriv inn passord" name="regPass"  type="text"> <br>
+                    Fornavn <input placeholder="Skriv inn fornavn" type="text" name="regFNavn"> <br>
+                    Etternavn <input placeholder="Skriv inn etternavn" type="text" name="regENavn"> <br>
+                    E-post <input placeholder="Skriv inn epost" type="email" name="regEpost"> <br>
+                    Fødselsdato <input type="date" name="regFDato"> <br>
+                    Telefonummer <input placeholder="Skriv inn telefonummer" type="tel" name="regTlf"> <br>';
+                } elseif($_GET["Type"]=="arbeidsgiver"){
+                    echo 'Brukernavn <input placeholder="Skriv inn brukernavn" name="regBNavn" type="text"> <br>
+                    Passord <input placeholder="Skriv inn passord" name="regPass"  type="text"> <br>
+                    Firmanavn <input placeholder="Skriv inn firmanavn" type="text" name="regFirmaNavn"> <br>
+                    Ledernavn <input placeholder="Skriv inn Ledernavn" type="text" name="regLederNavn"> <br>
+                    E-post <input placeholder="Skriv inn epost" type="email" name="regEpost"> <br>
+                    Telefonummer <input placeholder="Skriv inn telefonummer" type="tel" name="regTlf"> <br>';
+                }
+            ?>
             <button type="submit">Send inn</button>
         </form>
     </div>
