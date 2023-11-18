da26019afc2a2163843616107115fa00d1f65aac
Sander Aadal <sanderla@uia.no>
2023-11-18 16:40:16 +0100

    La til DB Funksjon OpenDB, CloseDB, QuerySelectSpesBruker, QuerySelectRolleFromBruker

--- www/Assets/Lib/PHPFunctions/db.php
+++ www/Assets/Lib/PHPFunctions/db.php
@@ -7,11 +7,10 @@
 </head>
 <body>
     <?php
-
-    //This file is a db initialization file and will be included in every file requiring a db connection
+function OpenDBConnection(){
     $username = "root"; #Legger inn en variabel for å skrive inn brukernavn i myphpadmin
     $password = ""; #Legger inn variabel for passordet i phpadmin
-    $servername = "127.0.0.1:4306"; #Lager variabel "host" for servernavn, må være port og IP hos OSKAR
+    $servername = "localhost"; #Lager variabel "host" for servernavn, må være port og IP hos OSKAR
     $database = "jobbsystem"; #Lager variabel for databasenavnet
     $conn = new mysqli($servername, $username,$password, $database); #Etablerer variabel for koblingen til sql serveren
 
@@ -20,7 +19,50 @@
         exit();
     }
     echo "Connection success"; //Successful connection
-    $conn->close(); //Lukker koblingen for å hindre memory leak
+    return $conn;
+}
+
+function CloseDBConnection($conn) {
+    $conn->close(); // Close the connection to prevent memory leaks
+}
+
+function QuerySelectSpesBruker($conn) { //Fetch SpesifikkBruker for sjekk om Verdien finnes fra før i Tabell-Bruker
+    $brukerNavn = 'X'; 
+    // SQL query
+    $sql = "SELECT * FROM Bruker WHERE Brukernavn = '$brukerNavn'";
+
+    // Execute the query
+    $result = $conn->query($sql);
+        if ($result) {
+            if ($result->num_rows > 0) {
+                echo "Brukernavnet '$brukerNavn' finnes allerede!";
+            } else {
+                echo "Ingen brukere med '$brukerNavn' veldig bra! WIP";
+            }
+          }
+        else {
+            echo "Big Fail" . $conn->error;
+        }
+}
+
+function QuerySelectRolleFromBruker($conn) { //Fetch Rolle i Bruker-Tabellen
+    $brukerNavn = 'X'; 
+    // SQL query
+    $sql = "SELECT Rolle FROM Bruker WHERE Brukernavn = '$brukerNavn'";
+
+    // Execute the query
+    $result = $conn->query($sql);
+        if ($result) {
+            if ($result->num_rows > 0) {
+                // Fetch the value from the result set
+                $row = $result->fetch_assoc();
+                $rolle = $row['Rolle'];
+                return $rolle;
+            }}
+        else {
+            echo "Big Fail" . $conn->error;
+        }
+}
 
     ?>  
 </body>
