5f87790e2b7f3534d5b626e7d844a6c7f526cc60
Oskar <Oskar.hauge123@gmail.com>
2023-11-20 12:12:12 +0100

    Endret på registreringsstylen

--- www/Pages/Registrer/Registrer.php
+++ www/Pages/Registrer/Registrer.php
@@ -8,23 +8,23 @@
 </head>
 <body id="startBody">
     <div id="regFormBox">
-        <form action="" method="POST" id="regForm">
+        <form action="/Jobbsystem/www/Assets/Lib/PHPFunctions/RegistrerProsess.php?Type=<?php echo $_GET["Type"]?>" method="POST" id="regForm">
             <?php 
                 if($_GET["Type"]=="arbeidssoker"){
-                    echo 'Brukernavn <input class="inputBox" placeholder="Skriv inn brukernavn" name="regBNavn" type="text"> <br>
-                    Passord <input class="inputBox" placeholder="Skriv inn passord" name="regPass"  type="text"> <br>
-                    Fornavn <input class="inputBox" placeholder="Skriv inn fornavn" type="text" name="regFNavn"> <br>
-                    Etternavn <input class="inputBox" placeholder="Skriv inn etternavn" type="text" name="regENavn"> <br>
-                    E-post <input class="inputBox" placeholder="Skriv inn epost" type="email" name="regEpost"> <br>
-                    Fødselsdato <input class="inputBox" type="date" name="regFDato"> <br>
-                    Telefonummer <input class="inputBox" placeholder="Skriv inn telefonummer" type="tel" name="regTlf"> <br>';
+                    echo 'Brukernavn <input required class="inputBox" placeholder="Skriv inn brukernavn" name="regBNavn" type="text"> <br>
+                    Passord <input required class="inputBox" placeholder="Skriv inn passord" name="regPass"  type="password"> <br>
+                    Fornavn <input required class="inputBox" placeholder="Skriv inn fornavn" type="text" name="regFNavn"> <br>
+                    Etternavn <input required class="inputBox" placeholder="Skriv inn etternavn" type="text" name="regENavn"> <br>
+                    E-post <input required class="inputBox" placeholder="Skriv inn epost" type="email" name="regEpost"> <br>
+                    Fødselsdato <input required class="inputBox" type="date" name="regFDato"> <br>
+                    Telefonummer <input required class="inputBox" placeholder="Skriv inn telefonummer" type="tel" name="regTlf"> <br>';
                 } elseif($_GET["Type"]=="arbeidsgiver"){
-                    echo 'Brukernavn <input class="inputBox" placeholder="Skriv inn brukernavn" name="regBNavn" type="text"> <br>
-                    Passord <input class="inputBox" placeholder="Skriv inn passord" name="regPass"  type="text"> <br>
-                    Firmanavn <input class="inputBox" placeholder="Skriv inn firmanavn" type="text" name="regFirmaNavn"> <br>
-                    Ledernavn <input class="inputBox" placeholder="Skriv inn Ledernavn" type="text" name="regLederNavn"> <br>
-                    E-post <input class="inputBox" placeholder="Skriv inn epost" type="email" name="regEpost"> <br>
-                    Telefonummer <input class="inputBox" placeholder="Skriv inn telefonummer" type="tel" name="regTlf"> <br>';
+                    echo 'Brukernavn <input required class="inputBox" placeholder="Skriv inn brukernavn" name="regBNavn" type="text"> <br>
+                    Passord <input required class="inputBox" placeholder="Skriv inn passord" name="regPass"  type="password"> <br>
+                    Firmanavn <input required class="inputBox" placeholder="Skriv inn firmanavn" type="text" name="regFirmaNavn"> <br>
+                    Ledernavn <input required class="inputBox" placeholder="Skriv inn Ledernavn" type="text" name="regLederNavn"> <br>
+                    E-post <input required class="inputBox" placeholder="Skriv inn epost" type="email" name="regEpost"> <br>
+                    Telefonummer <input required class="inputBox" placeholder="Skriv inn telefonummer" type="tel" name="regTlf"> <br>';
                 }
             ?>
             <button>Send inn</button>
