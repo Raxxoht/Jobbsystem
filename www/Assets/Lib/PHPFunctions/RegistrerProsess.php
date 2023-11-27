1960f90638be18cf77fb527446248044ba910cc8
Oskar <Oskar.hauge123@gmail.com>
2023-11-27 14:00:13 +0100

    Første endring gjort i controller, den sjekker om brukernavnet eksisterer

--- www/Assets/Lib/PHPFunctions/RegistrerProsess.php
+++ www/Assets/Lib/PHPFunctions/RegistrerProsess.php
@@ -5,23 +5,27 @@ include "./db.php";
 if(isset($_GET["Type"]) && isset($_POST["regBNavn"])){
     $conn = OpenDBConnection();
     $curDate = date("Y-m-d H:i:s");
-    QueryInsertBruker($conn,$_POST["regBNavn"], $_POST["regPass"], $_GET["Type"], $curDate);
+    if(QuerySelectSpesBruker($conn, $_POST["regBNavn"])==true){
+        QueryInsertBruker($conn,$_POST["regBNavn"], $_POST["regPass"], $_GET["Type"], $curDate);
 
-    $assoc = QuerySelectAllBrukerInfo($conn, $_POST["regBNavn"], $_POST["regPass"]);
-
-    $brukerId=$assoc["BrukerID"];
-
-    if($_GET["Type"]=="Arbeidstaker"){
-        QueryInsertArbeidstaker($conn, $brukerId, $_POST["regFNavn"] . " " . $_POST["regENavn"], $_POST["regEpost"], $_POST["regFDato"], $_POST["regTlf"]);
-        header("Location: /Jobbsystem/www/index.php");
-
-    } elseif($_GET["Type"]=="Arbeidsgiver"){
-        QueryInsertArbeidsgiver($conn, $brukerId, $_POST["regFirmaNavn"], $_POST["regLederNavn"], $_POST["regEpost"], $_POST["regTlf"]);
-         header("Location: /Jobbsystem/www/index.php");
+        $assoc = QuerySelectAllBrukerInfo($conn, $_POST["regBNavn"], $_POST["regPass"]);
+    
+        $brukerId=$assoc["BrukerID"];
+    
+        if($_GET["Type"]=="Arbeidstaker"){
+            QueryInsertArbeidstaker($conn, $brukerId, $_POST["regFNavn"] . " " . $_POST["regENavn"], $_POST["regEpost"], $_POST["regFDato"], $_POST["regTlf"]);
+            header("Location: /Jobbsystem/www/index.php");
+    
+        } elseif($_GET["Type"]=="Arbeidsgiver"){
+            QueryInsertArbeidsgiver($conn, $brukerId, $_POST["regFirmaNavn"], $_POST["regLederNavn"], $_POST["regEpost"], $_POST["regTlf"]);
+             header("Location: /Jobbsystem/www/index.php");
+        } else {
+            header("Location: /Jobbsystem/www/index.php");
+        }
+        CloseDBConnection($conn);
     } else {
-        header("Location: /Jobbsystem/www/index.php");
+        header("Location: /Jobbsystem/www/index.php?BNavn=Tatt");
     }
-    CloseDBConnection($conn);
 } else {
     header("Location: /Jobbsystem/www/index.php");
 }
