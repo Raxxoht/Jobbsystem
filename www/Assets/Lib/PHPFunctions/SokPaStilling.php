793bea98667a853a0e5a80417fa6df781226186b
Sander Aadal <sanderla@uia.no>
2023-11-29 22:50:36 +0100

    CVsjekk lagt til

--- www/Assets/Lib/PHPFunctions/SokPaStilling.php
+++ www/Assets/Lib/PHPFunctions/SokPaStilling.php
@@ -7,10 +7,12 @@ include $_SERVER["DOCUMENT_ROOT"] . "/Jobbsystem/www/Assets/Lib/PHPFunctions/db.
 include $_SERVER["DOCUMENT_ROOT"] . "/Jobbsystem/www/Assets/Lib/Klasser/arbeidstaker.php";
 include $_SERVER["DOCUMENT_ROOT"] . "/Jobbsystem/www/Assets/Lib/Klasser/arbeidsgiver.php";
 
+$conn = OpenDBConnection();
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $Soknadtekst = $_POST['SoknadTekst'];
     $Tittel = $_POST['Tittel'];
-    
+
     if (isset($_GET['BrukerID'])) {
         $BrukerID = $_GET['BrukerID'];
     }
@@ -19,15 +21,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $JobbannonseID = $_GET['JobbannonseID'];
     }
 
+    if(isset($_SESSION["Bruker"])) {    
+        $object = unserialize($_SESSION["Bruker"]);
+        $BnavnAG = $object->Brukernavn;
+        $assocs = QuerySelectProfilforAT($conn, $BnavnAG);
+        $ArbeidsInfo = $assocs[0];
+        $Profil = $assocs[1];
+    }
+
     $DateTime = date('Y-m-d H:i:s');
-    print_r($DateTime);
 
-    $conn = OpenDBConnection();
-    QueryInsertSoknad($conn, $Soknadtekst, $Tittel, $DateTime, $BrukerID, $JobbannonseID);
-    CloseDBConnection($conn);
+    $SpesAnnonse = QuerySelectSpesAnnonse($conn, $JobbannonseID);
 
-    header("Location: http://localhost/Jobbsystem/www/Pages/Stilling/Stilling.php");
-    exit();
+    if ($SpesAnnonse["KravCV"] == 1 && $ArbeidsInfo["CV"] !== NULL) {
+        QueryInsertSoknad($conn, $Soknadtekst, $Tittel, $DateTime, $BrukerID, $JobbannonseID);
+        CloseDBConnection($conn);
+        header("Location: http://localhost/Jobbsystem/www/Pages/Stilling/Stilling.php");
+        exit();
+    } elseif ($SpesAnnonse["KravCV"] == 0) {
+        QueryInsertSoknad($conn, $Soknadtekst, $Tittel, $DateTime, $BrukerID, $JobbannonseID);
+        CloseDBConnection($conn);
+        header("Location: http://localhost/Jobbsystem/www/Pages/Stilling/Stilling.php");
+        exit();
+    } else {
+        echo "Mangler CV for å Søke på denne stilling";
+    }
 } else {
     // If the form is not submitted, handle accordingly
     echo "Form not submitted";
