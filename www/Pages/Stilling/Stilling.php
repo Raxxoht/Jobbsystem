e547ce091fd896187645f360db90aa8c0fc8668f
Oskar <Oskar.hauge123@gmail.com>
2023-11-30 04:09:31 +0100

    Stylet annonsene ferdig og gjorde det smud

--- www/Pages/Stilling/Stilling.php
+++ www/Pages/Stilling/Stilling.php
@@ -28,60 +28,44 @@ if(isset($_SESSION["Bruker"])){
 
 $conn = OpenDBConnection();
 $AnnonseListe = QuerySelectAllAnnonser($conn);
-CloseDBConnection($conn); //Easter Egg
+function sjekkKrav($krav){
+    if($krav==1){
+        return "Green";
+    } elseif($krav==0){
+        return "Red";
+    }
+}
 
 ?>
-<?php foreach ($AnnonseListe as $Annonse): ?>
-    <a href="Pages/Stilling/SpesifikkStilling.php?JobbannonseID=<?= $Annonse['JobbannonseID'] ?>">
-        <table border="1">
-            <thead>
-                <tr>
-                    <th>Tittel</th>
-                    <th>ArbeidsgiverID</th>   
-                    <th>Beskrivelse</th> 
-                    <th>KravCV</th>   
-                    <th>KravDoc</th>   
-                    <th>KravTekst</th>     
-                    <th>Tidsfrist</th>                  
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td><?= $Annonse['Tittel'] ?></td>
-                    <td><?= $Annonse['ArbeidsgiverID'] ?></td>
-                    <td><?= $Annonse['Beskrivelse'] ?></td>
-                    <td><?= $Annonse['KravCV'] ?></td>
-                    <td><?= $Annonse['KravDoc'] ?></td>
-                    <td><?= $Annonse['KravTekst'] ?></td>
-                    <td><?= $Annonse['Tidsfrist'] ?></td>
-
-                </tr>
-            </tbody>
-        </table>
-        </a>
-<?php endforeach; ?>
-
-
-<!-- JEG BRUKER DET UNDER OMRÅDET TIL Å SKETCHE ANNONSER OG HVORDAN DE KOMMER TIL Å SE UT, TRENGER SIKKERT LITT FEEDBACK FRA MR SANDERSON PÅ DENNE -->
-    <div class="annonseBoks">
-        <div class="spaceBoks">
-            <div class="annonseHeader">
-                <h2 class="annonseTittel">Vaktmester</h2>
-                <h2 class="annonseLeder">Jordan Blaben</h2>
-            </div>
-            <div class="Hovedinnhold">
-                <p class="annonseBeskrivelse">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec cursus felis. Maecenas vitae augue a odio dictum accumsan ac in nulla. Maecenas malesuada elit et nisi posuere ullamcorper. In hac habitasse platea dictumst. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce aliquet magna eu mauris sollicitudin, quis varius risus facilisis. Phasellus volutpat commodo leo et porta. Pellentesque facilisis mauris eget pulvinar placerat. Pellentesque ornare elementum venenatis. In eget urna tempor, malesuada nunc a, convallis odio. Nunc quis ante ut nunc sollicitudin mattis vel maximus magna. Pellentesque suscipit in justo a dictum. Maecenas eget nulla velit.</p>
-            </div>
-            <div class="Footer">
-                <div class="annonsekravBoks">CV</div>
-                <div class="annonsekravBoks">DOC</div>
-                <div class="annonsekravBoks">TEKST</div>
-            </div>
-            <div class="annonseknappBoks">
-                <button class="annonseknapp">Se stilling</button>
-                <button class="annonseknapp">Søk på stilling</button>
+<?php foreach ($AnnonseListe as $Annonse): //Kul løsning ?> 
+    <?php
+    $sql = "Select LederNavn from arbeidsgiver where ArbeidsGiverID =" . $Annonse["ArbeidsgiverID"];
+    $giverInfo = $conn->query($sql);
+    $row = $giverInfo->fetch_assoc();
+    $Navn = $row["LederNavn"];
+    ?>
+    <div class="senterBoks">
+        <div class="annonseBoks">
+            <div class="spaceBoks">
+                <div class="annonseHeader">
+                    <h2 class="annonseTittel"><?=$Annonse["Tittel"]?></h2>
+                    <h2 class="annonseLeder"><?=$Navn?></h2>
+                </div>
+                <div class="Hovedinnhold">
+                    <p class="annonseBeskrivelse"><?=$Annonse["Beskrivelse"]?></p>
+                </div>
+                <div class="Footer">
+                    <div style="background-color:<?=sjekkKrav($Annonse["KravCV"])?>;" class="annonsekravBoks">CV</div>
+                    <div style="background-color:<?=sjekkKrav($Annonse["KravDoc"])?>;" class="annonsekravBoks">DOC</div>
+                    <div style="background-color:<?=sjekkKrav($Annonse["KravTekst"])?>;" class="annonsekravBoks">TEKST</div>
+                </div>
+                <div class="annonseknappBoks">
+                    <a href="/Jobbsystem/www/Pages/Stilling/SpesifikkStilling.php?"><button class="annonseknapp">Se stilling</button></a>
+                    <button class="annonseknapp">Søk på stilling</button>
+                </div>
             </div>
         </div>
     </div>
+<?php endforeach;CloseDBConnection($conn); ?>
 </body>
 </html>
