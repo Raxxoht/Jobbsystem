6329211511cbfce9420d3c32894bfd894ed38f63
Sander Aadal <sanderla@uia.no>
2023-12-02 11:03:33 +0100

    Refaktorering av RegProsess

--- www/Assets/Lib/PHPFunctions/RegistrerProsess.php
+++ www/Assets/Lib/PHPFunctions/RegistrerProsess.php
@@ -7,39 +7,48 @@ if(isset($_GET["Type"]) && isset($_POST["regBNavn"]) && isset($_POST["regPass"])
     $type = $_GET["Type"];
     $conn = OpenDBConnection();
     $curDate = date("Y-m-d H:i:s");
+    $regBNavn = $_POST["regBNAVN"];
+    $regPass = $_POST["regPass"];
+    $regTlf = $_POST["regTlf"];
+    $regFNavn = $_POST["regFNavn"];
+    $regENavn = $_POST["regENavn"];
+    $regFDato = $_POST["regFDato"];
+    $regFirmaNavn = $_POST["regFirmaNavn"];
+    $regLederNavn = $_POST["regLederNavn"];
+    $regEpost =  $_POST["regEpost"];
     
-    if(QuerySelectSpesBruker($conn, $_POST["regBNavn"])==1){
+    if(QuerySelectSpesBruker($conn, $regBNavn)==1){
         header("Location: /Jobbsystem/www/Pages/Registrer/Registrer.php?BNavn=Tatt&Type=$type");
 
-    } elseif(passordVal($_POST["regPass"])!="Bra"){
-        $passMelding = passordVal($_POST["regPass"]);
+    } elseif(passordVal($regPass)!="Bra"){
+        $passMelding = passordVal($regPass);
         header("Location: /Jobbsystem/www/Pages/Registrer/Registrer.php?Type=$type&passMelding=$passMelding");
 
-    } elseif(tlfVal($_POST["regTlf"])!="Bra"){
-        $tlfMelding = tlfVal($_POST["regTlf"]);
+    } elseif(tlfVal($regTlf)!="Bra"){
+        $tlfMelding = tlfVal($regTlf);
         header("Location: /Jobbsystem/www/Pages/Registrer/Registrer.php?Type=$type&tlfMelding=$tlfMelding");
         
-    } elseif(navnVal($_POST["regFNavn"])!="Bra" OR navnVal($_POST["regENavn"])!="Bra"){
-        if(navnVal($_POST["regFNavn"])!="Bra"){$navnMelding = navnVal($_POST["regFNavn"]);}
-        elseif(navnVal($_POST["regENavn"])!="Bra"){$navnMelding = navnVal($_POST["regENavn"]);}
+    } elseif(navnVal($regFNavn)!="Bra" OR navnVal($regENavn)!="Bra"){
+        if(navnVal($regFNavn)!="Bra"){$navnMelding = navnVal($regFNavn);}
+        elseif(navnVal($regENavn)!="Bra"){$navnMelding = navnVal($regENavn);}
         header("Location: /Jobbsystem/www/Pages/Registrer/Registrer.php?Type=$type&navnMelding=$navnMelding");
 
-    } elseif(fDatoVal($_POST["regFDato"])!="Bra"){
-        $datoMelding = fDatoVal($_POST["regFDato"]);
+    } elseif(fDatoVal($regFDato)!="Bra"){
+        $datoMelding = fDatoVal($regFDato);
         header("Location: /Jobbsystem/www/Pages/Registrer/Registrer.php?Type=$type&datoMelding=$datoMelding");
     } else {
-        QueryInsertBruker($conn,$_POST["regBNavn"], $_POST["regPass"], $_GET["Type"], $curDate);
+        QueryInsertBruker($conn, $regBNavn, $regPass, $type, $curDate);
 
-        $assoc = QuerySelectAllBrukerInfo($conn, $_POST["regBNavn"], $_POST["regPass"]);
+        $assoc = QuerySelectAllBrukerInfo($conn, $regBNavn, $regPass);
     
         $brukerId=$assoc["BrukerID"];
     
-        if($_GET["Type"]=="Arbeidstaker"){
-            QueryInsertArbeidstaker($conn, $brukerId, $_POST["regFNavn"] . " " . $_POST["regENavn"], $_POST["regEpost"], $_POST["regFDato"], $_POST["regTlf"]);
+        if($type=="Arbeidstaker"){
+            QueryInsertArbeidstaker($conn, $brukerId, $regFNavn . " " . $regENavn, $regEpost, $regFDato, $regTlf);
             header("Location: /Jobbsystem/www/index.php");
     
-        } elseif($_GET["Type"]=="Arbeidsgiver"){
-            QueryInsertArbeidsgiver($conn, $brukerId, $_POST["regFirmaNavn"], $_POST["regLederNavn"], $_POST["regEpost"], $_POST["regTlf"]);
+        } elseif($type=="Arbeidsgiver"){
+            QueryInsertArbeidsgiver($conn, $brukerId, $regFirmaNavn, $regLederNavn, $regEpost, $regTlf);
              header("Location: /Jobbsystem/www/index.php");
         } else {
             header("Location: /Jobbsystem/www/index.php");
