3e349ed421fb7bf13fdaad88b27cf70cdd5182ce
Sander Aadal <sanderla@uia.no>
2023-12-04 18:33:41 +0100

    sokefunk

--- www/Pages/StartSide/logout.php
+++ www/Pages/StartSide/logout.php
@@ -4,5 +4,6 @@ session_start();
 include_once $_SERVER["DOCUMENT_ROOT"] . "/Jobbsystem/www/Assets/Lib/PHPFunctions/login-sjekk.php";
 
 unset($_SESSION["Bruker"]);
+unset($_SESSION["sokListe"]);
 header("Location: /Jobbsystem/www/index.php");
 ?>
